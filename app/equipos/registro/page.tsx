2b5e87143dae8bc7abefb22bd8534034f7389848
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-04-21 08:39:51 -0500

    creando page registro y dashboard

---
+++ app/equipos/registro/page.tsx
@@ -0,0 +1,9 @@
+import FormularioRegistroEquipo from "@/components/equipos/FormularioRegistrarEquipo";
+
+export default function RegistroHojaVidaEquipoPage() {
+    return (
+        <div>
+            <FormularioRegistroEquipo/>
+        </div>
+    )
+}
